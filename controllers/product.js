6fe343e3d831b2d55186360b7ab37c0405e4a585
lucasdellamaggiore0 <dellamaggiorelucas25@gmail.com>
2022-09-01 10:10:00 -0300

    agregado de busqueda de productos por nombre

--- controllers/product.js
+++ controllers/product.js
@@ -1,11 +1,32 @@
-const {product, category} = require('../db/config')
+const { product } = require('../db/config')
+const { Op } = require('sequelize')
 
+async function getProducts(req, res) {
+    const { name } = req.query
+    try {
+        if (!name) {
+            const products = await product.findAll()
+            res.json({
+                products
+            })
+        } else {
+            const products = await  product.findAll({
+                where: {
+                    name: {
+                        [Op.like]: `%${name}%`
+                    }
+                }
+            })
+            res.json({
+                products
+            })
+        }
+    } catch (error) {
+        res.status(400).send({
+            error
+        })
+    }
 
-async function getProducts(req,res){
-    const products = await product.findAll()
-    res.json({
-        products
-    })
 }
 
 module.exports = {
