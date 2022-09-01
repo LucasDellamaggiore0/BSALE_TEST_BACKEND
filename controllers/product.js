4e61aa1cf0f0ef320c0cda175a0a6f2d21238fb5
lucasdellamaggiore0 <dellamaggiorelucas25@gmail.com>
2022-09-01 10:31:41 -0300

    agregado de filtro de productos por categoria

--- controllers/product.js
+++ controllers/product.js
@@ -2,19 +2,28 @@ const { product } = require('../db/config')
 const { Op } = require('sequelize')
 
 async function getProducts(req, res) {
-    const { name } = req.query
+    const { name, category } = req.query
     try {
-        if (!name) {
+        if (!name && !category) {
             const products = await product.findAll()
             res.json({
                 products
             })
-        } else {
-            const products = await  product.findAll({
+        } else if (name && !category) {
+            const products = await product.findAll({
                 where: {
                     name: {
                         [Op.like]: `%${name}%`
                     }
+                }
+            })
+            res.json({
+                products
+            })
+        }else{
+            const products = await product.findAll({
+                where:{
+                    category: `${category}`
                 }
             })
             res.json({
