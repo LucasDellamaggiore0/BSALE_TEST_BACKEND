7635dadabd5da1c8cf53642df80740487c749cf8
lucasdellamaggiore0 <dellamaggiorelucas25@gmail.com>
2022-09-03 15:21:03 -0300

    arreglando bugs para deploy

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const {handleConnection} = require('./db/config')
 
 
 
-    server.listen(3000, ()=>{
+    server.listen(process.env.PORT || 3000, ()=>{
         console.log('Conectado en el puerto 3000')
     })
     handleConnection();
